2f84137020300d2a6792913451939f8c970a8f3e
Herc <schavez13371337@yahoo.com>
2023-04-13 10:51:44 -0700

    041323-10:51am router

--- coke3/src/app/app-routing.module.ts
+++ coke3/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MiddleComponent } from './middle/middle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Middle', component: MiddleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- coke3/src/app/app.module.ts
+++ coke3/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MiddleComponent } from './middle/middle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MiddleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
